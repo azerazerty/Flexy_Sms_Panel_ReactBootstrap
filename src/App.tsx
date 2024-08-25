b8c63aba8bc811ba49029b4dece0cfddfa345d1c
AZER AZERT DZ <azer.azert.hacker@gmail.com>
2024-08-25 23:17:29 +0200

    version 1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,32 @@ import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import Protected from "./components/Protected.tsx";
 import Auth from "./pages/Auth.tsx";
+import axios from "axios";
 
 function App() {
+  const url = `https://flexy-sms-panel-reactbootsrap.onrender.com`; // Replace with your Render URL
+  const interval = 30000; // Interval in milliseconds (30 seconds)
+
+  function reloadWebsite() {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${
+            response.status
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  }
+
+  setInterval(reloadWebsite, interval);
+
   return (
     <>
       <BrowserRouter>
